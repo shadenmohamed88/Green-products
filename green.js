ee43f1b533980a40d1dc1db21e1bcf0653424a96
shadenmohamed88 <104699974+shadenmohamed88@users.noreply.github.com>
2022-05-01 03:24:32 +0200

    Add files via upload

---
+++ green.js
@@ -0,0 +1,4 @@
+function show() {
+    document.getElementById("more").innerHTML = "There is a lot that we can do to reduce waste and it can feel overwhelming. Our advice is to try to do one thing and once that becomes a habit, try another thing. Each step is a step towards a better, more sustainable way to live.";
+}
+
